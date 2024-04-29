ab53af0ddf3ef1ba9e27a2233faa660f68d5b68f
syimleang <syimleang@myseneca.ca>
2024-04-28 20:09:03 -0400

    Add error handling and debouncing

--- script.js
+++ script.js
@@ -2,14 +2,40 @@ let imgBox = document.getElementById("imgBox");
 let qrImg = document.getElementById("qrImg");
 let qrText = document.getElementById("qrText");
 
+// QR Generate funcction using "qrserver" api
 const generateQR = () => {
   if (qrText.value.length > 0) {
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`;
-    imgBox.classList.add("showImg");
-  } else {
+    let qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`;
+
+    // Handle API url error
+    fetch(qrUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to generate QR code");
+        }
+        return response.blob();
+      })
+      .then((blob) => {
+        qrImg.src = URL.createObjectURL(blob);
+        imgBox.classList.add("showImg");
+      })
+      .catch((error) =>
+        console.error("QR code generate error: ", error.message)
+      );
+  }
+  // Input empty handling
+  else {
     qrText.classList.add("error");
     setTimeout(() => {
-        qrText.classList.remove("error");
+      qrText.classList.remove("error");
     }, 1000);
   }
 };
+
+// Debounce function
+const debounce = (func, delay) => {
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(func, delay);
+};
+
+qrText.addEventListener("input", () => debounce(generateQR, 500));
